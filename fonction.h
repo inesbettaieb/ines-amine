ff316d5789e60c55d145c9df52482b68e4715cd7
inesbettaieb <73528208+inesbettaieb@users.noreply.github.com>
2020-11-18 12:38:20 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,1 +1,24 @@
 
+int lire_fichier(char filename[],troupeau);
+void ajouter_animal(Troupeau * p_troupeau, int * p_index);
+void modifier_animal(Troupeau * p_troupeau, int * p_index);
+void supprimer_animal(Troupeau * p_troupeau, int * p_index);
+void chercher_animal(Troupeau * p_troupeau, int * p_index);
+void afficher_troupeaux(Troupeau * p_troupeau, int * p_index);
+int Nombre_brebis(Troupeau * p_troupeau, int * p_index);
+int Nombre_veaux(Troupeau * p_troupeau, int * p_index);
+
+
+typedef struct{
+int jour;
+int mois;
+int annee;
+}Date;
+
+typedef struct{
+int identifiant;
+char type[30];
+char sexe[30];
+int etat; // 1 bonne santé 0 malade
+Date date;
+}Troupeau;
